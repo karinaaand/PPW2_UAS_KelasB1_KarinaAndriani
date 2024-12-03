3c105fab308e275deb88980be6f61d9496fe09c9
karinaaand <karinaandriani@mail.ugm.ac.id>
2024-12-03 14:28:39 +0700

    bugfixing12

--- app/Http/Controllers/TransaksiDetailController.php
+++ app/Http/Controllers/TransaksiDetailController.php
@@ -12,22 +12,20 @@ class TransaksiDetailController extends Controller
 {
     public function index()
     {
-        $transaksidetail = TransaksiDetail::with('transaksi')->orderBy('id','DESC')->get();
-
-        return view('transaksidetail.index', );
+        $transaksidetail = TransaksiDetail::with('transaksi')->orderBy('id', 'DESC')->get();
+        return view('transaksidetail.index', compact('transaksidetail'));
     }
 
     public function detail(Request $request)
     {
         $transaksi = Transaksi::with('transaksidetail')->findOrFail($request->id_transaksi);
-
-        return view('transaksidetail.detail', );
+        return view('transaksidetail.detail', compact('transaksi'));
     }
 
     public function edit($id)
     {
         $transaksidetail = TransaksiDetail::findOrFail($id);
-        return view('transaksidetail.edit', );
+        return view('transaksidetail.edit', compact('transaksidetail'));
     }
 
     public function update(Request $request, $id)
@@ -38,32 +36,53 @@ class TransaksiDetailController extends Controller
             'jumlah' => 'required|numeric',
         ]);
 
-        // Gunakan transaction
+        DB::beginTransaction();
+
         try {
+            $transaksidetail = TransaksiDetail::findOrFail($id);
+
             $transaksidetail->nama_produk = $request->input('nama_produk');
             $transaksidetail->harga_satuan = $request->input('harga_satuan');
             $transaksidetail->jumlah = $request->input('jumlah');
-            $transaksidetail->subtotal = harga_satuan * jumlah
+            $transaksidetail->subtotal = $request->input('harga_satuan') * $request->input('jumlah');
+            $transaksidetail->save();
 
-            $transaksi->total_harga = sum subtotal
-            $transaksi->kembalian = bayar - total_harga; // hapus rumus
+            $transaksi = Transaksi::findOrFail($transaksidetail->id_transaksi);
+            $transaksi->total_harga = $transaksi->transaksidetail->sum('subtotal');
+            $transaksi->kembalian = $transaksi->bayar - $transaksi->total_harga;
+            $transaksi->save();
 
-            return redirect('transaksidetail/'.$transaksidetail->id_transaksi)->with('pesan', 'Berhasil mengubah data');
+            DB::commit();
+
+            return redirect()->route('transaksidetail.index', ['id_transaksi' => $transaksi->id])
+                             ->with('pesan', 'Berhasil mengubah data');
         } catch (\Exception $e) {
             DB::rollback();
-            return redirect()->back()->withErrors(['Transaction' => 'Gagal menambahkan data'])->withInput();
+            return redirect()->back()->withErrors(['Transaction' => 'Gagal mengubah data'])->withInput();
         }
     }
 
-    public function destroy()
+    public function destroy($id)
     {
-        $transaksidetail = TransaksiDetail::findOrFail($id);
+        DB::beginTransaction();
 
-        $transaksi = Transaksi::with('transaksidetail')->findOrFail($transaksidetail->id_transaksi);
-        $transaksi->total_harga = sum subtotal;
-        $transaksi->kembalian = bayar - total_harga;
-        $transaksi->save();
+        try {
+            $transaksidetail = TransaksiDetail::findOrFail($id);
 
-        return redirect('transaksidetail/'.$transaksidetail->id_transaksi)->with('pesan', 'Berhasil menghapus data');
+            $transaksi = Transaksi::findOrFail($transaksidetail->id_transaksi);
+            $transaksidetail->delete();
+
+            $transaksi->total_harga = $transaksi->transaksidetail->sum('subtotal');
+            $transaksi->kembalian = $transaksi->bayar - $transaksi->total_harga;
+            $transaksi->save();
+
+            DB::commit();
+
+            return redirect()->route('transaksidetail.index', ['id_transaksi' => $transaksi->id])
+                             ->with('pesan', 'Berhasil menghapus data');
+        } catch (\Exception $e) {
+            DB::rollback();
+            return redirect()->back()->withErrors(['Transaction' => 'Gagal menghapus data']);
+        }
     }
 }
