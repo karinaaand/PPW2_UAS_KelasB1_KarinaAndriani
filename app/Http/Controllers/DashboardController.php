1ebfcc7d2cfb4c12ca68c462a21189be4c89b018
karinaaand <karinaandriani@mail.ugm.ac.id>
2024-12-03 13:35:12 +0700

    bugfixing4

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaksi; 
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        $transaksi_count =
-        return view('dashboard', );
+
+        $transaksi_count = Transaksi::count();
+
+        return view('dashboard', ['transaksi_count' => $transaksi_count]);
     }
 }
